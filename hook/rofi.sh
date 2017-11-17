e68a2fc62daedab7e2ea1c594f43489f82fa56c2
Andrea Schiavini <metalelf0@gmail.com>
2017-11-17 16:03:23 +0100

    Base16 shell integration

---
+++ hook/rofi.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+rofi_config_file=$HOME/.config/rofi/config
+rofi_themes_dir=$HOME/sources/base16-rofi/themes
+
+rofi_theme_file=$rofi_themes_dir/base16-$BASE16_THEME.config
+if [ -f "$rofi_theme_file" ] && [ -f $rofi_config_file ]; then
+  cp "$rofi_theme_file" "$rofi_config_file"
+  echo 'Rofi theme updated'
+fi
